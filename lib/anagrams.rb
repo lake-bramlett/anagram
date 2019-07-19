4580868684f3632e2130ef3eca5f129063a0b34f
Lake Bramlett <lake.bramlett@gmail.com>
2019-07-19 11:18:12 -0700

    add separate script file

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -30,11 +30,4 @@ class Anagram
       p 'please enter a valid word'
     end
   end
-
 end
-# p 'Enter a word or phrase'
-# input_1 = gets.chomp
-# p 'Enter another word or phrase'
-# input_2 = gets.chomp
-# anagram = Anagram.new(input_1,input_2)
-# anagram.is_anagram?
---
+++ lib/anagrams_script.rb
@@ -0,0 +1,8 @@
+require_relative('./anagrams.rb')
+
+p 'Enter a word or phrase'
+input_1 = gets.chomp
+p 'Enter another word or phrase'
+input_2 = gets.chomp
+anagram = Anagram.new(input_1,input_2)
+anagram.is_anagram?
