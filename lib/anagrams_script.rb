a4ec90863cd70a1fbcceaf1b7d985613d17a9d96
Lake Bramlett <lake.bramlett@gmail.com>
2019-07-19 15:25:26 -0700

    allow user to successfully make API calls to oxford english dictionary

--- lib/anagrams_script.rb
+++ lib/anagrams_script.rb
@@ -8,11 +8,11 @@ client = OxfordDictionary.new(app_id: ENV['ID'], app_key: ENV['KEY'])
 while true
   p 'Enter a word or phrase'
   input_1 = gets.chomp
-  entry = client.entry(word: input_1, dataset: 'en-gb', params: {})
-  p entry.lexicalEntries.first
+  entry_1 = client.entry(word: input_1.downcase, dataset: 'en-gb', params: {}).results[0].lexicalEntries[0].entries[0].senses[0].definitions[0]
+  p "Definition of #{input_1}: #{entry_1}"
   p 'Enter another word or phrase'
   input_2 = gets.chomp
-  entry = client.entry(word: input_2, dataset: 'en-gb', params: {})
-  p entry.lexicalEntries.first
+  entry_2 = client.entry(word: input_2.downcase, dataset: 'en-gb', params: {}).results[0].lexicalEntries[0].entries[0].senses[0].definitions[0]
+  p "Definition of #{input_2}: #{entry_2}"
   anagram = Anagram.new(input_1,input_2).is_anagram?
 end
