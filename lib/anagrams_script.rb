8f22b4b1dd1ecb03d2733c2b7a16ad324d55d1d2
Lake Bramlett <lake.bramlett@gmail.com>
2019-07-19 12:26:36 -0700

    add looping script for on-going functionality

--- lib/anagrams_script.rb
+++ lib/anagrams_script.rb
@@ -1,8 +1,10 @@
 require_relative('./anagrams.rb')
 
-p 'Enter a word or phrase'
-input_1 = gets.chomp
-p 'Enter another word or phrase'
-input_2 = gets.chomp
-anagram = Anagram.new(input_1,input_2)
-anagram.is_anagram?
+while true
+  p 'Enter a word or phrase'
+  input_1 = gets.chomp
+  p 'Enter another word or phrase'
+  input_2 = gets.chomp
+  anagram = Anagram.new(input_1,input_2)
+  anagram.is_anagram?
+end
